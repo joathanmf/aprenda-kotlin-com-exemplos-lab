c2e060c7adc6de976dbff1a7f589c2d309cc307e
Joathan Mareto <joathanmareto@gmail.com>
2024-01-02 20:58:34 -0300

    Desafio Kotlin, refatorando código

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,75 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { FACIL, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>, val nivel: Nivel = Nivel.INTERMEDIARIO) {
 
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(vararg usuarios: Usuario) {
+        println("")
+        for (usuario in usuarios) {
+            if (inscritos.contains(usuario)) {
+                println("${usuario.nome} já está matriculado(a) na formação \"$nome\"\n")
+                continue
+            }
+            inscritos.add(usuario)
+            println("${usuario.nome} acabou de ser matriculado(a) na formação \"$nome\"")
+        }
+        println("")
+    }
+
+    fun desmatricular(usuario: Usuario) {
+        inscritos.remove(usuario)
+        println("\n${usuario.nome} cancelou sua matrícula na formação \"$nome\"\n")
+    }
+
+    fun listarMatriculados() {
+        println("Lista de alunos matriculados na formação \"$nome\"")
+        for (usuario in inscritos) {
+            val index = inscritos.indexOf(usuario) + 1
+            println("$index - ${usuario.nome}")
+        }
+    }
+
+    fun informacoes() {
+        println("\nInformações sobre a formação \"$nome\"")
+        println("Nível: $nivel")
+        println("CONTEÚDOS:")
+        for (conteudo in conteudos) {
+            println("- ${conteudo.nome} - ${conteudo.duracao} minutos")
+        }
+        println("")
     }
 }
 
+
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario("Joathan")
+    val usuario2 = Usuario("Paula")
+    val usuario3 = Usuario("Maria")
+    val usuario4 = Usuario("João")
+    val usuario5 = Usuario("José")
+
+    val conteudo1 = ConteudoEducacional("Conhecendo a documentação", 10)
+    val conteudo2 = ConteudoEducacional("Faça seu primeiro 'Olá, Mundo'", 15)
+    val conteudo3 = ConteudoEducacional("Todos os tipos da linguagem", 22)
+    val conteudo4 = ConteudoEducacional("Conhecendo o IntelliJ IDEA", 30)
+    val conteudo5 = ConteudoEducacional("Conhecendo o Kotlin Playground", 40)
+
+    val conteudos = listOf(conteudo1, conteudo2, conteudo3, conteudo4, conteudo5)
+
+    val formacao_kotlin = Formacao("Introdução a Kotlin", conteudos, Nivel.FACIL)
+
+    formacao_kotlin.matricular(usuario1, usuario2, usuario3, usuario4, usuario5)
+    formacao_kotlin.listarMatriculados()
+
+    formacao_kotlin.desmatricular(usuario3)
+    formacao_kotlin.listarMatriculados()
+
+    formacao_kotlin.informacoes()
 }
